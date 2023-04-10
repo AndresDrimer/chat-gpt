460e600cc87bddb5506dbeb437f7c467ac98efd4
AndresDrimer <andresdrimer@hotmail.com>
2023-04-10 20:45:01 -0300

    first commit

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,8 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head> <title>Let´s talk</title>
+      <link rel="shortcut icon" href="/crystal-ball-icon.ico" /></Head>
+      <body className='bg-gray-800'>
         <Main />
         <NextScript />
       </body>
